ebc8d0e0121fffd910e944d08c00054f610dd132
anjalichandran123 <anjalimeerachandran@gmail.com>
2024-07-24 13:01:49 +0530

    added1

--- ECOMMERCE/cart/admin.py
+++ ECOMMERCE/cart/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from cart.models import Cart,Order_table,Payment
+from django.http import HttpResponse
 admin.site.register(Cart)
 admin.site.register(Order_table)
 admin.site.register(Payment)
